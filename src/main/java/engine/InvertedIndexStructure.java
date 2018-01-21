bf8a4a15647259b5dd20fcc3c8b7f97420f283e4
Piotr Bobinski <piotr.bobinski@ericsson.com>
2018-01-21 18:08:17 +0100

    Introduce more private methods for improved readability[

--- src/main/java/engine/InvertedIndexStructure.java
+++ src/main/java/engine/InvertedIndexStructure.java
@@ -4,8 +4,8 @@ package engine;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
@@ -23,53 +23,6 @@ class InvertedIndexStructure {
         putIntoMappings(file);
     }
 
-    private void putIntoMappings(File file) {
-        try (Scanner scanner = new Scanner(file)) {
-            scanner.useDelimiter(" +");
-            while (scanner.hasNext()) {
-                String word = scanner.next();
-                if (mappings.containsKey(word) && mappings.get(word).stream().anyMatch(term -> term.getSource() == file)) {
-                    mappings.get(word).stream()
-                            .filter(it -> it.getSource().equals(file))
-                            .findFirst()
-                            .ifPresent(Term::addCount);
-                } else {
-                    mappings.put(word, new Term(file));
-                }
-            }
-        } catch (FileNotFoundException e) {
-            // TODO: logger.error() // warn() here
-            System.out.println("Error during opening file. ");
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * BufferedReader version for better performance in bigger documents
-     *
-     * @param query
-     * @return
-     */
-//    private void putIntoMappings(File file) {
-//        try (FileReader fr = new FileReader(file);
-//             BufferedReader reader = new BufferedReader(fr);) {
-//            String line = reader.readLine();
-//            while (line != null) {
-//                final String[] split = line.split(" ");
-//                Arrays.stream(split)
-//                        .forEach(s -> mappings.put(s, file));
-//                line = reader.readLine();
-//            }
-//        } catch (IOException e) {
-//            // TODO: logger.error() // warn() here
-//            System.out.println("IO Error: " + e.getLocalizedMessage());
-//            e.printStackTrace();
-//        }
-//    }
-    private List<Term> findPaths(String query) {
-        return (List<Term>) mappings.get(query);
-    }
-
     /**
      * Return the list of documents (filenames) where given term occurs sorted by tf-idf
      *
@@ -81,6 +34,72 @@ class InvertedIndexStructure {
         applyTfidfSort(terms);
         return getFilenames(terms);
     }
+
+    private void putIntoMappings(File file) {
+        try (Scanner scanner = new Scanner(file)) {
+            scanner.useDelimiter(" +");
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                if (mappings.containsKey(word) && wordComesFromFile(word, file)) {
+                    addCountInMapForWord(word, file);
+                } else {
+                    mappings.put(word, new Term(file));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // TODO: logger.error() // warn() here
+            System.out.println("Error during opening file. ");
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * BufferedReader version for better performance in bigger documents
+     *
+     * @param file
+     * @return
+     */
+    private void putIntoMappingsBuffered(File file) {
+        try (FileReader fr = new FileReader(file);
+             BufferedReader reader = new BufferedReader(fr);) {
+            String line = reader.readLine();
+            while (line != null) {
+                final String[] split = line.split(" ");
+                Arrays.stream(split)
+                        .forEach(word -> {
+                            if (mappings.containsKey(word) && wordComesFromFile(word, file)) {
+                                addCountInMapForWord(word, file);
+                            } else {
+                                mappings.put(word, new Term(file));
+                            }
+                        });
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            // TODO: logger.error() // warn() here
+            System.out.println("IO Error: " + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private boolean wordComesFromFile(final String word, final File file) {
+        return mappings.get(word)
+                .stream()
+                .anyMatch(term -> term.getSource() == file);
+    }
+
+    private void addCountInMapForWord(final String word, final File file) {
+        mappings.get(word).stream()
+                .filter(it -> it.getSource().equals(file))
+                .findFirst()
+                .ifPresent(Term::addCount);
+    }
+
+    private List<Term> findPaths(String query) {
+        return (List<Term>) mappings.get(query);
+    }
+
 
     private void applyTfidfSort(final List<Term> terms) {
         terms.sort(Comparator.comparingInt(Term::getNrOfOccurences).reversed());
